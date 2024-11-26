b468ad81119d690ce3bc0a4fe8f2bca27f371a5d
jalil <jalil@1001.digital>
2024-11-26 11:58:30 +0100

    Add title notice

--- contracts/contracts/Auctions.sol
+++ contracts/contracts/Auctions.sol
@@ -6,6 +6,8 @@ import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 
+/// @notice Freedom to transact.
+/// @author Visualize Value
 contract Auctions is ERC721Holder, ERC1155Holder {
     /// @notice The data struct for a given auction.
     struct Auction {
