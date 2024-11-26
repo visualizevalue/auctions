d0b10c28883cdb34f3d6a3a7b848ad19336d2d2e
jalil <jalil@1001.digital>
2024-11-26 11:02:10 +0100

    Remove unneeded code

--- contracts/contracts/Auctions.sol
+++ contracts/contracts/Auctions.sol
@@ -5,9 +5,6 @@ import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-
-import "hardhat/console.sol";
 
 struct Auction {
     address tokenContract;
