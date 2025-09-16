944bcbf5cf8e5b197147520d826b355a73073b72
jalil <jalil@1001.digital>
2025-09-16 16:52:07 +0200

    Add description to title component

--- app/components/Auction/Detail.client.vue
+++ app/components/Auction/Detail.client.vue
@@ -33,14 +33,6 @@
       <section class="details">
         <header class="title">
           <AuctionTokenTitle :token="token" />
-          <p v-if="token.description">
-            <ExpandableText
-              :text="token.description"
-              :length="95"
-              :expand-text="$t('read_more')"
-              :collapse-text="$t('collapse')"
-            />
-          </p>
           <!-- <p>Token Standard: ERC-{{ collection.tokenStandard }}</p> -->
           <!-- <p v-if="collection" class="artist"> -->
           <!--   {{ $t('token.by') }} <NuxtLink :to="{ name: 'id', params: { id: collection.owner } }">{{ store.displayName(collection.owner) }}</NuxtLink> -->
--- app/components/Auction/Token/Title.vue
+++ app/components/Auction/Token/Title.vue
@@ -2,6 +2,14 @@
   <h1>
     {{ token.name }} <small>#{{ token.tokenId }}</small>
   </h1>
+  <p v-if="token.description">
+    <ExpandableText
+      :text="token.description"
+      :length="95"
+      :expand-text="$t('read_more')"
+      :collapse-text="$t('collapse')"
+    />
+  </p>
 </template>
 
 <script setup lang="ts">
